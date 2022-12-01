4ac2b6d9fc3a49341aa55616e964c70eb705e00c
Ben <ben@finch.org.uk>
2022-12-01 11:35:47 +0000

    update shortest

--- CourseWork/Bilateral.hs
+++ CourseWork/Bilateral.hs
@@ -1,7 +1,9 @@
 -- any imports go here
 import Data.List ( permutations )
+import System.Win32 (COORD(x))
 
 {-Begin Question 2.1-}
+--O(n^2), length x itself
 number :: [Int] -> Int
 number [] = 0
 number (x:xs) = (10 ^ length xs * x) + number xs
@@ -10,9 +12,10 @@ number (x:xs) = (10 ^ length xs * x) + number xs
 {-End Question 2.1-}
 
 {-Begin Question 2.2-}
+--O(n^4) combine x firsthalf x secondhalf 
 splits :: [Int] -> [([Int],[Int])]
 splits [a,b] = [([a],[b])]
-splits xs =  combine (firsthalf xs []) (secondhalf xs)
+splits xs = combine (firsthalf xs []) (secondhalf xs)
 
 {-
 Arguments: 
@@ -22,6 +25,7 @@ Arguments:
     - add the current element to the current inner list
     - add the current inner list to the outer list of lists
     - when there are two elements, return the final inner list
+    - O(n^2) append x itself
 
 Output: a list of lists
 -}
@@ -29,7 +33,9 @@ firsthalf :: [Int] -> [Int] -> [[Int]]
 firsthalf [] [] = [[]]
 firsthalf [] ys = [[]]
 firsthalf [x,z] ys = [ys ++ [x]]
-firsthalf (x:xs) ys = (ys ++ [x]) : firsthalf xs (ys ++ [x])
+firsthalf (x:xs) ys = 
+    xys : firsthalf xs xys
+    where xys = ys ++ [x]
 
 {-
 Argument: a list of numbers
@@ -37,6 +43,7 @@ Argument: a list of numbers
   - add the tail of the list to the output
   - calculate the tail of the tail
   - when there are two elements left, give only the second
+  - O(n) itself
 
 Output: a list of lists
 -}
@@ -48,6 +55,7 @@ secondhalf (x:xs) = xs : secondhalf xs
 {-
 Arguments: two lists of lists of equal length
 Output: each corresponding list zipped togeather
+O(n)
 -}
 combine :: [[Int]] -> [[Int]] -> [([Int],[Int])]
 combine [] [] = []
@@ -79,6 +87,7 @@ Arguments: two lists of digits
     - turn both lists into numbers with "number"
     - multiply the numbers togeather
     - divide by ten until there is only one digit
+    - O(n^2), the algorithm is O(1) however using length twice makes it O(n^2)
 
 Output: the remaining digit
 -}
@@ -93,13 +102,14 @@ Arguments: to lists of digits
 
     - turn both lists to numbers with "number"
     - compare the size of both numbers
+    - O(1) because of "number"
 
 Output: the smaller of the two lists
 -}
-shortest :: [Int] -> [Int] -> [Int]
-shortest as bs
-    | number as < number bs = as
-    | otherwise = bs
+shortest :: Int -> Int -> Int
+shortest a b
+    | a < b = a
+    | otherwise = b
 
 {-
 Arguments: two lists of numbers
@@ -107,6 +117,7 @@ Arguments: two lists of numbers
     - turn both lists to numbers with "number"
     - multiply the numbers togeather
     - if the reversed number is the number, true
+    - O(n^2) reverse x show
 
 Output: true of the result is symmetric, otherwise false
 -}
@@ -130,12 +141,13 @@ Output: true of the pair is a possible solution
 -}
 isAcceptable :: ([Int],[Int]) -> Bool
 isAcceptable (as,bs)
-    | isPalendrome result && isFour == 4 && shortDigit == 3 = True
+    | isPalendrome (a*b) && isFour == 4 && shortDigit == 3 = True
     | otherwise = False
     where
-        result = number as * number bs
-        isFour = firstDigit as bs result
-        shortDigit = number (shortest as bs) `rem` 10
+        a = number as
+        b = number bs
+        isFour = firstDigit as bs (a*b)
+        shortDigit = shortest a b `rem` 10
 
 {-
 Arguments: none
@@ -144,7 +156,6 @@ Arguments: none
 
 Output: a list of all possible solutions
 -}
-
 acceptables :: [([Int],[Int])]
 acceptables = filter isAcceptable possibles
 
@@ -162,7 +173,7 @@ main = do
     -- print (isAcceptable ([7,6,1,3],[5,9,2,4,8])) -- False
     --print (splits [1..9])
     --print (length possibles)  -- 2903040 (from spec) (does run, but slowly)
-    --print (length acceptables)  
+    print (length acceptables)  
         -- test1: 2m 53s
         -- test2: stack overflow because of a typo
         -- test3: 2m 47s
