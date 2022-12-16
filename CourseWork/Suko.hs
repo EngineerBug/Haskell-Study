0c4571a8a0d1924a35a9e9092ddc7d0a4377c96d
EngineerBug <ben@finch.org.uk>
2022-12-16 12:50:10 +0000

    fixing suko

--- CourseWork/Suko.hs
+++ CourseWork/Suko.hs
@@ -18,14 +18,14 @@ acceptable t1 t2 t3 t4 us u vs v ws w grid =
        t1 == sum (indices [0,1,3,4] grid)
     && t2 == sum (indices [1,2,4,5] grid)
     && t3 == sum (indices [3,4,6,7] grid)
-    && t4 == sum (indices [5,6,8,9] grid)
-    && u == sum us
-    && v == sum vs
-    && w == sum ws
+    && t4 == sum (indices [4,5,7,8] grid)
+    && u == sum (indices us grid)
+    && v == sum (indices vs grid)
+    && w == sum (indices ws grid)
 
 --find the item in possible that matches the elements in acceptable
 suko :: Int -> Int -> Int -> Int -> [Int] -> Int -> [Int] -> Int -> [Int] -> Int -> [[Int]]
 suko t1 t2 t3 t4 us u vs v ws w = filter (acceptable t1 t2 t3 t4 us u vs v ws w) possibles
 
 main = do
-    print (suko 19 10 23 19 [7,2,3,6] 18 [4,1,9] 14 [8,5] 13)
+    print (suko 19 10 23 19 [0,1,2,3] 18 [4,5,8] 14 [6,7] 13)
